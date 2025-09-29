8b7643f2779c858d327a180d8ceffe2b566e5494
Samzid khan <mdsamjidkhan8@gmail.com>
2025-09-29 11:30:26 +0600

    admin dashbord update

---
+++ resources/views/backend/layouts/footer.blade.php
@@ -0,0 +1,27 @@
+        <footer class="footer">
+          <div class="container-fluid d-flex justify-content-between">
+            <nav class="pull-left">
+              <ul class="nav">
+                <li class="nav-item">
+                  <a class="nav-link" href="http://www.themekita.com">
+                    Online shop BD
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#"> Help </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#"> Licenses </a>
+                </li>
+              </ul>
+            </nav>
+            <div class="copyright">
+              {{ date('Y') }}, made with Samzid khan <i class="fa fa-heart heart text-danger"></i> by
+              <a href="http://www.onlineshop.com">Online || Shop BD</a>
+            </div>
+            <div>
+              Distributed by
+              <a target="_blank" href="https://onlineshop.com/">Shop</a>.
+            </div>
+          </div>
+        </footer>
